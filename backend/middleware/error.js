bf6477b71a0676347ab8eec46eaf02eb1cb1e1c0
hkumar0520 <53545303+hkumar0520@users.noreply.github.com>
2023-12-22 16:41:11 +0530

    Till 3hr 25 minutes
    Complete Backend Authentication

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -12,6 +12,29 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(message, 400);
     }
 
+    // Mongoose duplicate key error
+    if(err.code === 11000)
+    {
+        // "E11000 duplicate key error collection: ecommerceDB.users index: email_1 dup key: { email: \"dalchand4ud@gmail.com\" }"
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message, 400);
+
+    }
+
+    // Wrong jsonwebtoken error
+    if(err.name === 'JsonWebTokenError')
+    {
+        const message = `Json Web Token is invalid, try again`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // JWT Expire error
+    if(err.name === 'TokenExpiredError')
+    {
+        const message = `Json Web Token is Expired, try again`;
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+// built in module
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
 
@@ -65,6 +67,31 @@ userSchema.methods.getJWTToken = function(){
 userSchema.methods.comparePassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
 }
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function(){
+
+
+    // const resetToken = crypto.randomBytes(20).toString('hex');
+    // console.log(resetToken);
+    // const tokenCrypto = crypto.createHash('sha256').update(resetToken).digest('hex');
+    // console.log(tokenCrypto);
+
+    // Generating Token
+    // hex is for correctly displaying values 
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hasing and adding resetPasswordToken to userSchema
+    this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+    this.resetPasswordExpire = Date.now() + 15*60*1000;
+
+    return resetToken;
+};
+
 
 module.exports = mongoose.model("User", userSchema);
 
